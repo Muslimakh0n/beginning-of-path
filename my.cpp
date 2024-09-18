19b5bc0a74e68186c6b38e356d45a7eb7e28bdd0
Muslimakhon Shokirova <shokirovamuslimakhon@gmail.com>
2024-09-18 18:23:26 +0500

    yeey

--- my.cpp
+++ my.cpp
@@ -118,3 +118,17 @@ int main(){
    cout << "The area og rectangle is " << area << ".\n";
     return 0;
 }
+
+
+int main(){
+    int length, width, area;
+    
+    cout << "This program calculates the area of a ";
+    cout << "rectangle.\n";
+    cout << "Enter the length and width of the rectangle ";
+    cout << "separated by a space.\n";
+    cin >> length >> width;
+    area = length * width;
+    cout << "The area of the rectangle is " << area << endl;
+    return 0;
+}
