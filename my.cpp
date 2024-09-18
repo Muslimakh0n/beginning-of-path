6a30b88036ebfbf1b3d28ff300f1c9b8547c7a9f
Muslimakhon Shokirova <shokirovamuslimakhon@gmail.com>
2024-09-18 18:11:07 +0500

    also

--- my.cpp
+++ my.cpp
@@ -85,3 +85,22 @@ int main(){
     cout << "Wages for this week are $" << totalWages << endl;
     return 0;
 }
+
+int main(){
+    double regularWages,
+    basePayRate =18.25,
+    regularHours = 40.0,
+    overtimeWages,
+    overtimePayRate = 27.78,
+    overtimeHours = 10,
+    totalWages;
+    
+    regularWages = basePayRate * regularHours;
+    
+    overtimeWages = overtimePayRate * overtimeHours;
+    
+    totalWages = regularWages + overtimeWages;
+    
+    cout << "Wages for this week are $" << totalWages << endl;
+    return 0;
+}
